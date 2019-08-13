fbd2a633c43f2ee2ef26f0ad51496bf5f22f350f
gonzalezsoriano777 <RAFTB.Gonzalez@Outlook.com>
2019-08-13 13:39:48 -0500

    DayFive Proccess Activated.

---
+++ cSharp-Algorithms/HackerRank/DayFive.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cSharp_Algorithms.HackerRank
+{
+    public class DayFive
+    {
+        public int Loops(int num)
+        {
+
+
+
+            return num;
+        }
+    }
+}
