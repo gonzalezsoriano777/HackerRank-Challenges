5450ebfb521580beb201e6b6d0d4d810c0fd409c
gonzalezsoriano777 <RAFTB.Gonzalez@Outlook.com>
2019-09-26 11:38:52 -0500

    testing.

--- cSharp-Algorithms/CodeSignal/longestUnCorruptedSegments.cs
+++ cSharp-Algorithms/CodeSignal/longestUnCorruptedSegments.cs
@@ -8,6 +8,20 @@ namespace cSharp_Algorithms.CodeSignal
 {
     public class longestUnCorruptedSegments
     {
+        public int[] longestUncorruptedSegments()
+        {
+            int[] unCorruptedSegments = new int[2];
 
+            // testing array's streak of matching numbers and detecting any corrupted ( unmatching numbers )
+            int[] sourceArray = new int[] { 33531593, 96933415, 28506400, 39457872, 29684716, 86010806 };
+            int[] destinationArray = new int[] { 33531593, 96913415, 28506400, 39457872, 29684716, 86010806 };
+
+            int counter = 0;
+            int max = 0;
+
+           
+
+            return unCorruptedSegments; 
+        }
     }
 }
