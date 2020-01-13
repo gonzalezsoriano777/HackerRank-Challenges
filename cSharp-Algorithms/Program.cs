8ca8fff6fbbc5d2435afeaf0dff3d4b562a66f9f
gonzalezsoriano777 <RAFTB.Gonzalez@Outlook.com>
2020-01-13 14:02:29 -0600

    Starting over. With the 30 days of Code. THIS IS A RECAP...

--- cSharp-Algorithms/Program.cs
+++ cSharp-Algorithms/Program.cs
@@ -12,55 +12,7 @@ namespace cSharp_Algorithms
     {
         static void Main(string[] args)
         {
-            /*
-            #region DayTwo ObjectReference
-            DayTwo total = new DayTwo();
-            total.InitialCost();
-            #endregion
-
-            #region DayThree ObjectReference
-            DayThree LoopCalled = new DayThree();
-            LoopCalled.IfLoop(6);
-            #endregion
             
-            #region DayFour ObjectReference
-            DayFour Aging = new DayFour();
-            Aging.Age(15);
-            Aging.amIOld();
-            Aging.yearPassing();
-            #endregion
-
-            #region DayFive ObjectReference
-            DayFive IntegerLooping = new DayFive();
-            IntegerLooping.Loops(2);
-            #endregion
-
-            #region DaySix ObjectReference
-            DaySix WordParity = new DaySix();
-            WordParity.wordParity("Hacker");
-            #endregion
-
-
-            #region DaySeven Reversing Array
-            DaySeven arrReversing = new DaySeven();
-            arrReversing.arrReverse();
-            #endregion
-            
-
-            #region DayEight Dictionary&Maps
-            DayEight phoneBook = new DayEight();
-            phoneBook.ContactList();
-            #endregion
-            
-
-            #region DayNine Recursion
-            DayNine recursion = new DayNine();
-            recursion.factorial(2);
-            #endregion
-            */
-
-            longestUnCorruptedSegments match = new longestUnCorruptedSegments();
-            match.longestUncorruptedSegments(new int[4] { 7, 2, 3, 5 }, new int[4] { 7, 2, 2, 7 });
             
 
         }
