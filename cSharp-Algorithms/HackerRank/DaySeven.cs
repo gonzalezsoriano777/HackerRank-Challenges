82013b9cbd3017d9184f05fe8828def3e5f5f588
gonzalezsoriano777 <RAFTB.Gonzalez@Outlook.com>
2020-02-27 22:28:43 -0600

    Starting from day seven and forward.

--- cSharp-Algorithms/HackerRank/DaySeven.cs
+++ cSharp-Algorithms/HackerRank/DaySeven.cs
@@ -11,14 +11,7 @@ namespace cSharp_Algorithms.HackerRank
         
         public void Arrays()
         {
-            int[] numberArray = { 347, 19, 583, 83, 9, 3 };
-
-            Array.Reverse(numberArray);
-
-            foreach (int num in numberArray)
-            {
-                Console.WriteLine("{0}", num);
-            }
+            
 
         }
     }
