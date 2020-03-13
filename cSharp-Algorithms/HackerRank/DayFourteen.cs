323f1e3c67a19afed7191c9e81f29c4d8ff92e93
gonzalezsoriano777 <RAFTB.Gonzalez@Outlook.com>
2020-03-13 16:48:22 -0500

    Day Fourteen is complete.

---
+++ cSharp-Algorithms/HackerRank/DayFourteen.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cSharp_Algorithms.HackerRank
+{
+    public class Difference
+    {
+        private int[] elements;
+        private int maximumDifference;
+        
+        public Difference(int[] element)
+        {
+            this.elements = element;
+        }
+
+        public int computeDifference()
+        {
+            maximumDifference = elements.Max() - elements.Min();
+            return maximumDifference;
+        }
+
+    }
+}
