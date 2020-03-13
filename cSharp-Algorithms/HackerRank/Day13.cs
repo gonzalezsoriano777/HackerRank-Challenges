1e65a990d2333f01d654067541695680cb99dd7d
gonzalezsoriano777 <RAFTB.Gonzalez@Outlook.com>
2020-03-13 11:01:56 -0500

    Day 13 is complete!

--- cSharp-Algorithms/HackerRank/Day13.cs
+++ cSharp-Algorithms/HackerRank/Day13.cs
@@ -20,9 +20,34 @@ namespace cSharp_Algorithms.HackerRank
     }
 
 
-    class myBook : Book
+    class MyBook : Book
     {
-        
+        private int Price;
 
+        public MyBook(string title, string author, int price)
+                      :base(title, author)
+        {
+            this.Price = price;
+        }
+
+        public override void display()
+        {
+            Console.WriteLine("titleOfBook: {0}", title);
+            Console.WriteLine("authorOfBook: {0}", author);
+            Console.WriteLine("priceOfBook: {0}", Price);
+        }
+    
+    }
+
+    class Solution
+    {
+        static void MainFolder()
+        {
+            String title = Console.ReadLine();
+            String author = Console.ReadLine();
+            int price = Int32.Parse(Console.ReadLine());
+            Book new_novel = new MyBook(title, author, price);
+            new_novel.display();
+        }
     }
 }
