40102e6c017b23e62b30b814a97f21a981e9b8ef
gonzalezsoriano777 <RAFTB.Gonzalez@Outlook.com>
2020-03-13 10:51:41 -0500

    Day 11 complete. Had some trouble seeing the pattern for the hourglasses i would be going through. Solved it successfully.

---
+++ cSharp-Algorithms/HackerRank/Day13.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cSharp_Algorithms.HackerRank
+{
+    abstract class Book
+    {
+        protected string title;
+        protected string author;
+
+        public Book(string t, string a)
+        {
+            title = t;
+            author = a;
+        }
+        public abstract void display();
+    }
+
+
+    class myBook : Book
+    {
+        
+
+    }
+}
--- cSharp-Algorithms/HackerRank/DayEleven.cs
+++ cSharp-Algorithms/HackerRank/DayEleven.cs
@@ -24,6 +24,23 @@ namespace cSharp_Algorithms.HackerRank
             int counter = 0;
             var count = new List<int>();
 
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    top = arr[i][j] + arr[i][j + 1] + arr[i][j + 2];
+                    middle = arr[i + 1][j + 1];
+                    bottom = arr[i + 2][j] + arr[i + 2][j + 1] + arr[i + 2][j + 2];
+                    counter = top + middle + bottom;
+                    count.Add(counter);
+                }
+            }
+
+            count.Sort();
+            count.Reverse();
+
+            Console.WriteLine("{0}", count[0]);
+
                    
         }
     }
