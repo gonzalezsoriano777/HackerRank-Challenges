d674bc3c682f7b558eb5df04cf54cda85b0550dc
gonzalezsoriano777 <RAFTB.Gonzalez@Outlook.com>
2020-03-26 18:19:00 -0500

    Will be objectifying this day eventually.

--- cSharp-Algorithms/HackerRank/DayFifteen.cs
+++ cSharp-Algorithms/HackerRank/DayFifteen.cs
@@ -6,7 +6,40 @@ using System.Threading.Tasks;
 
 namespace cSharp_Algorithms.HackerRank
 {
-    class DayFifteen
+    public class Node
     {
+        public int data;
+        public Node next;
+        public Node(int d)
+        {
+            data = d;
+            next = null;
+        }
+
     }
+
+    class LinkedList
+    {
+
+        public static Node insert(Node head, int data)
+        {
+            //Complete this method
+            if (head == null)
+            {
+                return new Node(data);
+            }
+
+            if (head.next == null)
+            {
+                head.next = new Node(data);
+            }
+            else
+            {
+                insert(head.next, data);
+            }
+
+            return head;
+        }
+    }
+    
 }
