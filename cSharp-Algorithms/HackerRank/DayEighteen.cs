1c757fab61d7d059bb1e1a385ba25b9965ed4b10
gonzalezsoriano777 <RAFTB.Gonzalez@Outlook.com>
2020-03-30 19:14:34 -0500

    Day 18 Complete, will be objectifyed shortly.

--- cSharp-Algorithms/HackerRank/DayEighteen.cs
+++ cSharp-Algorithms/HackerRank/DayEighteen.cs
@@ -6,7 +6,33 @@ using System.Threading.Tasks;
 
 namespace cSharp_Algorithms.HackerRank
 {
-    class DayEighteen
+    class QueuesAndStacks
     {
+
+        public Stack<char> stack = new Stack<char>();
+        public Queue<char> queue = new Queue<char>();
+
+        public void pushCharacter(char ch)
+        {
+            stack.Push(ch);
+        }
+
+        public void enqueueCharacter(char ch)
+        {
+            queue.Enqueue(ch);
+        }
+
+        public char popCharacter()
+        {
+            char pop = stack.Pop();
+            return pop;
+        }
+
+        public char dequeueCharacter()
+        {
+            char deque = queue.Dequeue();
+            return deque;
+        }
+
     }
 }
