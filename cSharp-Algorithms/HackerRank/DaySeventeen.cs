66ad5398044c9e88b49372c66b8fc726ab2fe914
gonzalezsoriano777 <RAFTB.Gonzalez@Outlook.com>
2020-03-30 19:08:58 -0500

    Day Seventeen complete, will be objectifyed eventually.

--- cSharp-Algorithms/HackerRank/DaySeventeen.cs
+++ cSharp-Algorithms/HackerRank/DaySeventeen.cs
@@ -6,7 +6,17 @@ using System.Threading.Tasks;
 
 namespace cSharp_Algorithms.HackerRank
 {
-    class DaySeventeen
+    class MoreExceptions
     {
+        public int power(int n, int p)
+        {
+
+            if (n < 0 || p < 0)
+                throw new Exception("n and p should be non-negative");
+
+            double total = (int)Math.Pow(n, p);
+
+            return (int)total;
+        }
     }
 }
